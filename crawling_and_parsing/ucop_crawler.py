bdb5fc31b5a3518bc127b112cdecb0dcb3128792
albus <guptarohit994@gmail.com>
2019-11-12 16:45:14 -0800

    Making UCOP crawler generic for location and year

--- crawling_and_parsing/ucop_crawler.py
+++ crawling_and_parsing/ucop_crawler.py
@@ -1,136 +1,148 @@
-from selenium import webdriver
+from argparse import ArgumentParser
+from selenium.webdriver import Firefox
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.ui import WebDriverWait
 from ucop_webpage_parser import get_parsed_rows_ucop
 import csv
+from sys import stdout
+from os import path
 
 
+def do_ucop_crawl(location='San Diego', year=2018, headless=True, geckodriver_path='./geckodriver', output_dir="."):
+    '''
+    function to crawl the search results from the UCOP Salary webpage
 
-f = open('./../data/csv/ucop_san_diego_auto.csv', 'w')
-writer = csv.writer(f)
+    :param location: which location to use. default:'San Diego'
+    :type location: str
+    :param year: year for which wages need to be gathered. default:2018
+    :type year: int
+    :param headless: mode in which to launch browser, default:True
+    :type headless: bool
+    :param geckodriver_path: path to the geckodriver executable, default:'./geckodriver'
+    :type geckodriver_path: str
+    :param output_dir: dir where to place the generated csv, default:'../data/csv/ucop/''
+    :type output_dir: str
+    '''
+    assert isinstance(location,str), "location should be a string"
+    # would get stricter later
+    assert isinstance(year,int) and 0 <= year <= 2019, "year should be a int"
+    assert isinstance(headless,bool), "headless should be a bool"
+    assert isinstance(geckodriver_path,str) and path.exists(geckodriver_path), f"geckodriver does not exist at {geckodriver_path}"
+    assert isinstance(output_dir,str) and path.isdir(output_dir), f"Not a valid directory:{output_dir}"
 
+    options = Options()
+    options.headless = headless
 
-options = Options()
-options.headless = True
-driver = webdriver.Firefox(options=options, executable_path=r'./geckodriver')
-driver.get("https://ucannualwage.ucop.edu/wage/")
-print ("Headless Firefox Initialized")
-
-
-
-total_num_pages = 1
-pages_left = 1
-first_time = True
-
-while (pages_left > 0):
+    driver = Firefox(options=options, executable_path=geckodriver_path)
+    driver.get("https://ucannualwage.ucop.edu/wage/")
     
-    if (first_time):
-        # write header
-        writer.writerow(['SNo','Year','Location','FirstName','LastName','Title',
-                         'GrossPay','RegularPay','OvertimePay','OtherPay'])
+    print ('Firefox Initialized in %s mode'%('headless' if headless else 'head'))
+           
+    # select location
+    select = Select(driver.find_element_by_id('location'))
+    options_locations = [o.text for o in select.options]
+
+    if 'Select a Location' in options_locations:
+        options_locations.remove('Select a Location')
+
+    assert location in options_locations, f"location can be {options_locations}"
+    select.select_by_visible_text(location)
+
+    # select year
+    #['2018', '2017', '2016', '2015', '2014', '2013', '2012', '2011', '2010']
+    select = Select(driver.find_element_by_id('year'))
+    options_years = [o.text for o in select.options]
+
+    assert str(year) in options_years, f"year can be {options_years}"
+    select.select_by_visible_text(str(year))
+
+    # select 60 results to be viewed per page
+    results_per_page = 60
+    select = Select(driver.find_element_by_id('pager2').find_element_by_class_name('ui-pg-selbox'))
+    options_results_per_page =  [o.text for o in select.options]
+
+    assert str(results_per_page) in options_results_per_page, f"results_per_page can only be {options_results_per_page}"
+    select.select_by_visible_text(str(results_per_page))
+
+    # click on search
+    driver.find_element_by_id('searchButton').click()
+
+    # # wait for click to complete, else time out
+    # WebDriverWait(driver, 60, poll_frequency=60).until(lambda x: x.find_element_by_id('load_list2').is_displayed())
+
+    # wait until results are being loaded
+    # TODO unclean, try using above commented code
+    while (driver.find_element_by_id('load_list2').is_displayed()):
+        pass
+
+    name_for_file = '_'.join(location.lower().split())
+    f = open(f"{output_dir}/./ucop_{name_for_file}_{year}_auto.csv", 'w')
+    #f = open(f"../../data/csv/ucop/ucop_{name_for_file}_{year}_auto.csv", 'w')
+    writer = csv.writer(f)
+
+    # write header
+    writer.writerow(['SNo','Year','Location','FirstName','LastName','Title',
+                     'GrossPay','RegularPay','OvertimePay','OtherPay'])
+
+    # get how many paged result has appeared
+    total_num_pages = int(driver.find_element_by_id('sp_1').text)
+    pages_left = total_num_pages
+    
+    if total_num_pages < 1:
+        assert False, "Search yielded no result or wait for results to load expired."
+    
+    while (pages_left > 0):
         
-        # select San Diego as Location
-        select = Select(driver.find_element_by_id('wrapper').\
-                               find_element_by_id('content').\
-                               find_element_by_id('sform').\
-                               find_element_by_id('location'))
-        # print(select.options)
-        # print([o.text for o in select.options]) # these are string-s
-        select.select_by_visible_text('San Diego')
-
-        # select 60 results to be viewed per page
-        select_60 = Select(driver.find_element_by_id('wrapper').\
-                           find_element_by_id('content').
-                           find_element_by_id('gbox_list2').\
-                           find_element_by_id('pager2').\
-                           find_element_by_class_name('ui-pg-table').\
-                           find_element_by_class_name('ui-pg-selbox')
-                          )
-        select_60.select_by_visible_text('60')
-
-        # click on search
-        driver.find_element_by_id('wrapper').\
-               find_element_by_id('content').\
-               find_element_by_id('sform').\
-               find_element_by_id('searchButton').click()
+        # need to click on next and wait from 2nd page onwards
+        if (pages_left != total_num_pages):
+            driver.find_element_by_id('next').click()
             
-#         # wait for click to complete, else time out
-#         WebDriverWait(driver, 60, poll_frequency=60).until(lambda x: x.find_element_by_id('wrapper').
-#                                                                        find_element_by_id('content').
-#                                                                        find_element_by_id('load_list2').\
-#                                                                        is_displayed()
-#                                                           )
-
-        while (driver.find_element_by_id('wrapper').
-                      find_element_by_id('content').
-                      find_element_by_id('load_list2').\
-                      is_displayed()
-              ):
-            pass
-        
-        # get how many paged result has appeared
-        total_num_pages = int(driver.
-                              find_element_by_id('wrapper').\
-                              find_element_by_id('content').\
-                              find_element_by_id('pg_pager2').\
-                              find_element_by_id('sp_1').\
-                              text
-                             )
-        
-        if (total_num_pages >= 1):
-            # have already parsed this page
-            pages_left = total_num_pages - 1
-            print(f"Need to go through {pages_left} more pages!")
+            # wait until results are being loaded
+            # TODO unclean, try using above commented code
+            while (driver.find_element_by_id('load_list2').is_displayed()):
+                pass
             
-            # strict = false since results may be less than 60 on 1st page
-            try:
-                rows = get_parsed_rows_ucop(driver.page_source, strict=False)
-                for row in rows:
-                    writer.writerow(row)
-            except AssertionError as e:
-                print(f"caught exception {e} in get_parsed_rows_ucop!")
-                #no need to write row in this case
-        else:
-            assert False, "Search yielded no result or wait for results to load expired. Maybe exit?"
+            assert total_num_pages == int(driver.find_element_by_id('sp_1').text), "Something went wrong! total_num_pages has changed now."
         
-        #set first time as false
-        first_time = False
-    else:
-        #click on next
-        driver.find_element_by_id('wrapper').\
-               find_element_by_id('content').\
-               find_element_by_id('pg_pager2').\
-               find_element_by_id('next').click()
-        
-        while (driver.find_element_by_id('wrapper').
-                       find_element_by_id('content').
-                       find_element_by_id('load_list2').\
-                       is_displayed()
-              ):
-            pass
-        
-        assert total_num_pages == int(driver.find_element_by_id('wrapper').\
-                                             find_element_by_id('content').\
-                                             find_element_by_id('pg_pager2').\
-                                             find_element_by_id('sp_1').text),\
-                          "Something went wrong! total_num_pages has changed now."
-
-        pages_left -= 1
-        print(f"Need to go through {pages_left} more pages!")
+        # strict = false since results may be less than 60 on 1st page
         try:
-            if (pages_left == 0):
-                # last page, results may be less than 60 => strict = False
-                rows = get_parsed_rows_ucop(driver.page_source, strict=False)
-            else:
-                rows = get_parsed_rows_ucop(driver.page_source)
+            rows = get_parsed_rows_ucop(driver.page_source, strict=False)
             for row in rows:
                 writer.writerow(row)
         except AssertionError as e:
             print(f"caught exception {e} in get_parsed_rows_ucop!")
             #no need to write row in this case
-#close f
-f.close()
+        
+        pages_left -=  1
+        stdout.write('\r')
+        stdout.write(f"Need to go through {pages_left} more pages!")
+        stdout.flush()
+
+    print("")
+    
+    #close f
+    f.close()
+    
+    driver.quit()
 
 
-driver.quit()
+if __name__ == "__main__":
+    parser = ArgumentParser(description='Crawls information from UCOP Annual Wages webpage')
+    parser.add_argument('--location', type=str, nargs='?', default='San Diego',
+                        help='location whose wages to crawl, default:\'San Diego\'')
+    parser.add_argument('--year', type=int, nargs='?', default=2018,
+                        help='year for which data to crawl, default:2018')
+    parser.add_argument('--headless', type=bool, nargs='?',default=True,
+                        help='mode in which to launch browser, default:True')
+    parser.add_argument('--geckodriver_path', type=str, nargs='?', default='./geckodriver',
+                        help='path to the geckodriver executable, default:\'./geckodriver\'')
+    parser.add_argument('--output_dir', type=str, nargs='?', default='../data/csv/ucop/',
+                        help='dir where to place the generated csv, default:\'../data/csv/ucop/\'')
+    args = parser.parse_args()
+
+    print(f"year:{args.year}, location:{args.location}, headless:{args.headless}, geckodriver_path:{args.geckodriver_path}, output_dir:{args.output_dir}")
+
+    print("=========================================================================================")
+    do_ucop_crawl(location=args.location, year=args.year, headless=args.headless, geckodriver_path=args.geckodriver_path, output_dir=args.output_dir)
+    print("=========================================================================================")
