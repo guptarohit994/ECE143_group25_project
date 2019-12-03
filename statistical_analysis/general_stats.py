417a62562b594659225e5de39325204c0f483156
Onat <57125532+onatucsd@users.noreply.github.com>
2019-12-03 01:23:42 -0800

    summarize_professor_salary_data function added.

--- statistical_analysis/general_stats.py
+++ statistical_analysis/general_stats.py
@@ -1,5 +1,6 @@
 import helper
 import numpy as np
+import pandas as pd
 
 def get_general_statistics(name, dept_list_to_use):
     """Prints out the general statistics for the requested departments
@@ -28,3 +29,26 @@ def get_general_statistics(name, dept_list_to_use):
         print("{} mean response rate: {}".format(name, mean_respond_rate))
         print("{} median response rate: {}".format(name, median_respond_rate))
         print("{} response rate std: {}".format(name, std_respond_rate))
+        
+        
+import helper_salary as helper
+
+def summarize_professor_salary_data(uc_name):
+    """
+    This function takes uc school name (san diego, los angeles, berkeley) and prints some statistics about it.
+    : param uc_name: (list), uc school name
+    """
+    #assert uc_name in ['sandiego', 'losangeles', 'berkeley'], "you should enter sandiego, losangeles or berkeley as school names"
+    
+    print('UC ' + uc_name)
+    salary_data = helper.read_data(uc_name)
+    salary_data = helper.calculate_netsalary(salary_data)
+    
+    salary_data_professors = salary_data[salary_data['Title'].str.contains("PROF")]
+    total_payment = salary_data_professors['TotalPay'];
+    print('Total number of professors: ' + str(len(salary_data_professors))) 
+    
+    print('Mean: ' + str("%.2f" % total_payment.mean()) + '$');  
+    print('Max: ' + str(total_payment.max()) + '$');  
+    print('Min: ' + str(total_payment.min()) + '$');  
+    print("_____________________________\n");
