f39a001eeb2fe885e794a8e8737bdc36534aa37b
Onat <57125532+onatucsd@users.noreply.github.com>
2019-12-02 23:16:14 -0800

    Minor changes made

--- statistical_analysis/helper_salary.py
+++ statistical_analysis/helper_salary.py
@@ -22,3 +22,20 @@ def calculate_netsalary(salary_data):
     salary_data[['RegularPay', 'OvertimePay', 'OtherPay']] = salary_data[['RegularPay', 'OvertimePay', 'OtherPay']].apply(pd.to_numeric)  
     salary_data['TotalPay'] = salary_data['RegularPay'] + salary_data['OvertimePay'] + salary_data['OtherPay']
     return salary_data
+
+def create_mean_salaries(uc_schools):
+    """
+    This function takes a list of uc schools and it returns mean salary values.
+    : param uc_schools: (list), uc schools
+    : return: mean_salaries: (list), mean salary values for specified uc schools
+    """
+    assert len(uc_schools) > 0
+    
+    mean_salaries = list()
+    for school in uc_schools:
+        salary_data = read_data(school)
+        salary_data = calculate_netsalary(salary_data)
+        salary_data_professors = salary_data[salary_data['Title'].str.contains("PROF")]
+        total_payment = salary_data_professors['TotalPay'];
+        mean_salaries.append(int(total_payment.mean())/1000) 
+    return mean_salaries
