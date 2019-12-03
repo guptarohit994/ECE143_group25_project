e2dee50c7ea048120ca8726d8ef23c22c1935795
Onat <57125532+onatucsd@users.noreply.github.com>
2019-12-02 23:15:44 -0800

    Refactored

--- statistical_analysis/barcharts.py
+++ statistical_analysis/barcharts.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import helper_salary as helper
 
 def create_mean_salary_barchart(uc_schools):
     """
@@ -9,7 +10,7 @@ def create_mean_salary_barchart(uc_schools):
     assert len(uc_schools) > 0
     
     y_pos = np.arange(len(uc_schools))
-    mean_salary = create_mean_salaries(uc_schools)
+    mean_salary = helper.create_mean_salaries(uc_schools)
 
     fig, ax = plt.subplots(figsize=(10,5))
     plt.bar(y_pos, mean_salary, align='center', alpha=0.5)
@@ -19,20 +20,3 @@ def create_mean_salary_barchart(uc_schools):
     plt.yticks(size = 12)
 
     plt.show()
-
-def create_mean_salaries(uc_schools):
-    """
-    This function takes a list of uc schools and it returns mean salary values.
-    : param uc_schools: (list), uc schools
-    : return: mean_salaries: (list), mean salary values for specified uc schools
-    """
-    assert len(uc_schools) > 0
-    
-    mean_salaries = list()
-    for school in uc_schools:
-        salary_data = read_data(school)
-        salary_data = calculate_netsalary(salary_data)
-        salary_data_professors = salary_data[salary_data['Title'].str.contains("PROF")]
-        total_payment = salary_data_professors['TotalPay'];
-        mean_salaries.append(int(total_payment.mean())/1000) 
-    return mean_salaries
